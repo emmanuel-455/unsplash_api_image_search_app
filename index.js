538045be03ae589e66a8d22531d5f0617cf1615c
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-01-02 00:59:50 +0100

    update

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ async function searchImages() {
     if (page === 1) {
         searchResults.innerHTML = ""
     }
+    result.map((result) => {
+        const imageWrapper = document.createElement("div")
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement("img")
+        image.src = result.urls.small;
+        image.alt = result.alt_description
+        const imageLink = document.createElement("a")
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
 
+        imageWrapper.appendChild(image);
+        imageLink.appendChild(imageLink)
+        imageWrapper.appendChild(imageWrapper)
+
+        page++
+        if (page > 1) {
+            showMore.style.display = "block"
+        }
+    })
 
 }
