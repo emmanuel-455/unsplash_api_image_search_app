1ba14d0fc88a29a5c9dd0aba5daaa0cb4d7ad8ad
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-01-03 00:41:37 +0100

    udpade

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const accessKey = "NYfSCKaFzBsACHT9ENZ70OBT_NnBbsAG6l4Ep1YaTyU";
 const formEl = document.querySelector("form")
 const inputEl = document.getElementById("search-input");
-const searchResults = document.querySelector(".search-result")
+const searchResults = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
 
 let inputData = "";
@@ -9,34 +9,42 @@ let page = 1;
 
 async function searchImages() {
     inputData = inputEl.value;
-    const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
 
-    const response = await fetch(URL);
-    const Data = await response.json()
-    const result = Data.result
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
 
     if (page === 1) {
-        searchResults.innerHTML = ""
+        searchResults.innerHTML = "";
     }
-    result.map((result) => {
-        const imageWrapper = document.createElement("div")
-        imageWrapper.classList.add("search-result")
-        const image = document.createElement("img")
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
         image.src = result.urls.small;
-        image.alt = result.alt_description
-        const imageLink = document.createElement("a")
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
         imageLink.href = result.links.html;
         imageLink.target = "_blank";
         imageLink.textContent = result.alt_description;
 
         imageWrapper.appendChild(image);
-        imageLink.appendChild(imageLink)
-        imageWrapper.appendChild(imageWrapper)
-
-        page++
-        if (page > 1) {
-            showMore.style.display = "block"
-        }
-    })
-
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+    page++;
+    if (page > 1) {
+        showMore.style.display = "block";
+    }
 }
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMore.addEventListener("click", () => {
+    searchImages();
+})
